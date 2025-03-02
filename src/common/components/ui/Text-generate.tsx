d30db8c69b091d59564dac9351815c5a3f7ceaa7
muhammadwahaarifin <wahabmuhammad691@gmail.com>
2025-03-03 01:17:54 +0700

    remove

--- src/common/components/ui/Text-generate.tsx
+++ src/common/components/ui/Text-generate.tsx
@@ -38,7 +38,7 @@ export const TextGenerateEffect = ({
                 delay: stagger(0.1),
             }
         );
-    }, [scope.current]);
+    }, [animate, duration, filter]);
 
     const renderWords = () => {
         return (
